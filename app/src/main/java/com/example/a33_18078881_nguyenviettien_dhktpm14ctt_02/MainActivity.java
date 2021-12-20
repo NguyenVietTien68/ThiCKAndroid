df1dbdf5b1c3fbc7c4e7ba71cdb42dec1abe3715
LAPTOP <tiencuongkhoa71@gmail.com>
2021-12-20 18:16:43 +0700

    Cau 1 b: Dang nhap voi google

---
+++ app/src/main/java/com/example/a33_18078881_nguyenviettien_dhktpm14ctt_02/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.a33_18078881_nguyenviettien_dhktpm14ctt_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+    int RC_SIGN_IN = 0;
+    GoogleSignInClient mGoogleSignInClient;
+    SignInButton signin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signin = findViewById(R.id.sign_in_button);
+        signin.setOnClickListener(view->{
+            signIn();
+        });
+    }
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Intent intent = new Intent(MainActivity.this, ScreenTransfer.class);
+            startActivity(intent);
+            Toast.makeText(this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+
+        } catch (ApiException e) {
+            Log.w("ERRor", "signInResult:failed code=" + e.getStatusCode());
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/a33_18078881_nguyenviettien_dhktpm14ctt_02/ScreenTransfer.java
@@ -0,0 +1,14 @@
+package com.example.a33_18078881_nguyenviettien_dhktpm14ctt_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScreenTransfer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_transfer);
+    }
+}
